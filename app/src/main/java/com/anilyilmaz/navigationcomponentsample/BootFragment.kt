402580cd3e9f98bd1c27b9d11c58d5ecde964d97
ANIL YILMAZ <uyelikanil@gmail.com>
2021-05-05 16:47:40 +0300

    Add boot fragment

---
+++ app/src/main/java/com/anilyilmaz/navigationcomponentsample/BootFragment.kt
@@ -0,0 +1,27 @@
+package com.anilyilmaz.navigationcomponentsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class BootFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_boot, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.boot_navigate_home_button)
+        button?.setOnClickListener { findNavController().navigate(R.id.action_boot_to_home) }
+    }
+}
--- app/src/main/java/com/anilyilmaz/navigationcomponentsample/HomeFragment.kt
+++ app/src/main/java/com/anilyilmaz/navigationcomponentsample/HomeFragment.kt
@@ -5,11 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 
-/**
- * Fragment used to show how to navigate to another destination
- */
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
@@ -22,23 +21,26 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.navigate_boot_button)
+        button?.setOnClickListener { findNavController().navigate(R.id.bootFragment) }
     }
 
     override fun onStart() {
         super.onStart()
 
-        Toast.makeText(context, "Home Fragment on start", Toast.LENGTH_SHORT)
+        Toast.makeText(context, "Home Fragment on start", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStop() {
         super.onStop()
 
-        Toast.makeText(context, "Home Fragment on stop", Toast.LENGTH_SHORT)
+        Toast.makeText(context, "Home Fragment on stop", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        Toast.makeText(context, "Home Fragment on destroy", Toast.LENGTH_SHORT)
+        Toast.makeText(context, "Home Fragment on destroy", Toast.LENGTH_SHORT).show()
     }
 }
